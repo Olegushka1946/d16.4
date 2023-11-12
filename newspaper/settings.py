ada5e9ae68852da7faf67a2797c0a14c1d93613e
Olegushka1946 <126393443+Olegushka1946@users.noreply.github.com>
2023-11-12 23:50:21 +0300

    settings.py

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -255,7 +255,7 @@ LOGGING = {
         },
         'mail_admins': {
             'level': 'ERROR',
-            'filters': ['require_debug_false'],
+            'filters': ['require_debug_true'],
             'class': 'django.utils.log.AdminEmailHandler',
             'formatter': 'mail'
         },
